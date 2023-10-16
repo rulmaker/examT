5ced86d3827542b85af3bdf7e1028497eebc4dea
rulmaker <rauljaciel_91@hotmail.com>
2023-10-16 15:15:51 -0600

    added Log4j2 from lombok

--- src/main/java/com/examTaker/rjsm/controllers/student/StudentController.java
+++ src/main/java/com/examTaker/rjsm/controllers/student/StudentController.java
@@ -2,6 +2,7 @@ package com.examTaker.rjsm.controllers.student;
 
 import com.examTaker.rjsm.business.student.StudentServiceImpl;
 import com.examTaker.rjsm.models.Student;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/product")
+@Log4j2
 public class StudentController {
     private final StudentServiceImpl studentService;
 
@@ -24,8 +26,10 @@ public class StudentController {
         List<Student> students = studentService.getAllStudents();
 
         if(students.isEmpty()){
+            log.warn("There are no students registered");
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No existen estudiantes registrados");
         }else{
+            log.info("Student list generated succesfully");
             return ResponseEntity.ok(students);
         }
     }
@@ -37,8 +41,10 @@ public class StudentController {
         Optional<Student> student = studentService.getStudentById(id);
 
         if(student.isPresent()){
+            log.info("Returning student with id: {}", student.get().getStudent_id());
             return ResponseEntity.ok(student.get());
         }else{
+            log.warn("Student with id: {}, not found.", student.get().getStudent_id());
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Estudiante no encontrado");
         }
     }
@@ -48,6 +54,7 @@ public class StudentController {
     public ResponseEntity<Student> createStudent(@RequestBody Student student){
         Student studentSaved = studentService.createStudent(student);
 
+        log.info("Student with id: {} created.", studentSaved.getStudent_id());
         return ResponseEntity.status(HttpStatus.CREATED).body(studentSaved);
 
     }
